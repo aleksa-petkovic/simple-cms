8a89ee6e9e6f21a5787ac8f463815012129e209f
aleksa <onshiftmusic@gmail.com>
2020-07-16 14:00:09 +0200

    Create navigation class, factory and service provider

---
+++ src/Library/Navigation/ServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Library\Navigation;
+
+use Library\Navigation\Factory;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * @inheritDoc
+     */
+    public function register(): void
+    {
+        $this->app->singleton(Factory::class, static function (Application $app): Factory {
+            return new Factory();
+        });
+    }
+}
