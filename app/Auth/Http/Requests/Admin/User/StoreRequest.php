0471ddb0c3be59ea20efb34bae640b43b43f2be3
aleksa <onshiftmusic@gmail.com>
2020-07-17 14:07:22 +0200

    Create user store, update and delete request

---
+++ app/Auth/Http/Requests/Admin/User/StoreRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Http\Requests\Admin\User;
+
+use App\Auth\Role\Repository as RoleRepository;
+use App\Http\Requests\Request;
+
+class StoreRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @param RoleRepository $roleRepository A role repository instance.
+     *
+     * @return array
+     */
+    public function rules(RoleRepository $roleRepository): array
+    {
+        $roles = implode(',', array_keys($roleRepository->getOptions()));
+
+        return [
+            'email' => [
+                'required',
+                'email',
+                'unique:users',
+            ],
+            'first_name' => [
+                'required',
+            ],
+            'last_name' => [
+                'required',
+            ],
+            'role' => [
+                'required',
+                "in:{$roles}",
+            ],
+        ];
+    }
+}
