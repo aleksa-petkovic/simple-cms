b0734ec3b5f32ff77ba5812734770a97a9e11ff2
aleksa <onshiftmusic@gmail.com>
2020-07-22 08:31:11 +0200

    Add templates for pages

---
+++ resources/views/templates/pages/news.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.front')
+
+@section('content')
+
+<div class="container">
+    <h1>{!! $page->title !!}</h1>
+    @if ( ! $page->articles->isEmpty())
+    <div class="row">
+        @foreach($articles as $article)
+        <?php $url = URL::action('App\Content\Http\Controllers\Front\Page\Controller@resolveRoute', ['any' => $article->full_slug]); ?>
+        <div class="col-sm-8">
+            @if($article->getImage('main'))
+            <img src="{{ URL::to($article->getImage('main')) }}" class="card-img-top" height="200">
+            @endif
+            <h2>{{ $article->title }}</h2>
+            <h5>{!! $article->lead !!}</h5>
+            <a href="{{ $url }}" style="-webkit-text-fill-color: gray">{{ trans('buttons.readMore') }}</a>
+        </div>
+            @endforeach
+    </div>
+        @endif
+</div>
+
+@stop
